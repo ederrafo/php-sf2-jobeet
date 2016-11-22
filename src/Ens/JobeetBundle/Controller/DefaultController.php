9f5e4537694fa04dc16a9140ab7f70d0bfcf2a64
Eder Pariona <e.pariona@costamar.com>
2016-11-22 23:07:54 +0000

    Day 13 Security

--- src/Ens/JobeetBundle/Controller/DefaultController.php
+++ src/Ens/JobeetBundle/Controller/DefaultController.php
@@ -3,6 +3,9 @@
 namespace Ens\JobeetBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+# Add for login
+use Symfony\Component\Security\Core\SecurityContext;
+
 
 class DefaultController extends Controller
 {
@@ -10,4 +13,25 @@ class DefaultController extends Controller
     {
         return $this->render('EnsJobeetBundle:Default:index.html.twig');
     }
+
+    public function loginAction()
+    {
+    	$request = $this->getRequest();
+        $session = $request->getSession();
+ 
+        // get the login error if there is one
+        if ($request->attributes->has(SecurityContext::AUTHENTICATION_ERROR)) {
+            $error = $request->attributes->get(SecurityContext::AUTHENTICATION_ERROR);
+        } else {
+            $error = $session->get(SecurityContext::AUTHENTICATION_ERROR);
+            $session->remove(SecurityContext::AUTHENTICATION_ERROR);
+        }
+ 
+        return $this->render('EnsJobeetBundle:Default:login.html.twig', array(
+            // last username entered by the user
+            'last_username' => $session->get(SecurityContext::LAST_USERNAME),
+            'error'         => $error,
+        ));
+    }
+
 }
